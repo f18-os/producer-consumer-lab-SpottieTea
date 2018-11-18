175fb9e9f438b5b3cd4ae30003f3c9b61202e759
SpottieTea <noashea.d@gmail.com>
2018-11-17 17:21:34 -0700

    expanding extraction thread.

--- ProConThreads.py
+++ ProConThreads.py
@@ -1,9 +1,20 @@
 import threading
+import cv2
+import numpy as np
+import queue
 
 
-class threads(Thread):
+class threadExtract(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.start()
     def run(self):
+
+        name = "client.mp4"
+        
+        #frame number
+        fCount = 0;
+
+        #open file
+        vidFile = cv2.VideoCapture()
     
